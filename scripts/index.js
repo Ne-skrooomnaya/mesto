e55f393fd336ac62e5ecc6064e35309238cd7fd7
ne_skrooomnaya <angelf04032005@gmail.com>
2022-01-12 17:43:21 +0300

    готово

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+
+const editButton = document.querySelector(".profile__edit_button");
+const profileName = document.querySelector(".profile__name");
+const profileHobby = document.querySelector(".profile__hobby");
+const popupForm = document.querySelector('.popup');
+const closeButton = document.querySelector('.popup__close');
+const inputForm = document.querySelector('.popup__form');
+const inputName = document.querySelector('#popup__name');
+const inputHobby = document.querySelector('#popup__hobby');
+
+
+function openPopup() {
+    popupForm.classList.add('popup_opened');
+    inputName.value = userName.textContent;
+    inputHobby.value = userHobby.textContent;
+}
+
+function closePopup() {
+    popupForm.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    userName.textContent = inputName.value;
+    userHobby.textContent = inputHobby.value;
+    closePopup();
+}
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+inputForm.addEventListener('submit', formSubmitHandler);
